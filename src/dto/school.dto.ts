6d6c9be0a209fdf8aa9d4ce8c23a51290bc02d5e
Luis Mendez <dluismendezdj@gmail.com>
2024-04-11 19:32:18 -0400

    Validate dto data

--- src/dto/school.dto.ts
+++ src/dto/school.dto.ts
@@ -1,11 +1,29 @@
+import { IsBoolean, IsNotEmpty, IsOptional, IsString } from "class-validator";
+
 export class SchoolDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
   address: string;
+
+  @IsBoolean()
+  @IsOptional()
   isActive?: boolean;
 }
 
 export class USchoolDto {
+  @IsString()
+  @IsOptional()
   name?: string;
+
+  @IsString()
+  @IsOptional()
   address?: string;
+
+  @IsBoolean()
+  @IsOptional()
   isActive?: boolean;
 }
