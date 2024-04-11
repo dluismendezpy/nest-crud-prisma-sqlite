9fec95fe3a3fc1004c5ddae50d7608736eea99dc
Luis Mendez <dluismendezdj@gmail.com>
2024-04-11 19:20:50 -0400

    Create controller actions for school module

--- src/school/school.controller.ts
+++ src/school/school.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from "@nestjs/common";
+import { SchoolService } from "./school.service";
+import { School } from "@prisma/client";
+import { SchoolDto, USchoolDto } from "../school.dto";
 
 @Controller("school")
-export class SchoolController {}
+export class SchoolController {
+  constructor(private readonly schoolService: SchoolService) {}
+
+  @Get()
+  async getSchools(): Promise<School[]> {
+    return this.schoolService.getSchools();
+  }
+
+  @Get(":id")
+  async getSchool(@Param("id") id: string): Promise<School> {
+    return this.schoolService.getSchool({ id: Number(id) });
+  }
+
+  @Post()
+  async createSchool(@Body() school: SchoolDto): Promise<School> {
+    return this.schoolService.createSchool({ school });
+  }
+
+  @Patch(":id")
+  async updateSchool(
+    @Param("id") id: string,
+    @Body() school: USchoolDto,
+  ): Promise<School> {
+    return this.schoolService.updateSchool({ id: Number(id), school });
+  }
+
+  @Delete(":id")
+  async deleteSchool(@Param("id") id: string): Promise<School> {
+    return this.schoolService.deleteSchool({ id: Number(id) });
+  }
+}
