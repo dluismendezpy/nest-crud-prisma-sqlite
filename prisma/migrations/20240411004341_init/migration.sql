fd9eb9585b772ccefd9685337c2d7967fab811ac
Luis Mendez <dluismendezdj@gmail.com>
2024-04-10 20:44:37 -0400

    Create school model and migrate

---
+++ prisma/migrations/20240411004341_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "School" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "address" TEXT,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
